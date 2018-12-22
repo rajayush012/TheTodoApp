e267ff05891a1d037ce838dc0d9ba2263f7eb68d
Ayush Raj <raj.ayush012@gmail.com>
2018-12-22 12:12:22 +0530

    All set and running

--- list/views.py
+++ list/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Category, Todo
 
 # Create your views here.
 def home(request):
-    return render(request,'list/home.html')
+    categories = Category.objects
+    todos = Todo.objects
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['desc'] and request.POST['due'] and request.POST['category']:
+            todo = Todo()
+            todo.task = request.POST['title']
+            todo.desc = request.POST['desc']
+            todo.due = request.POST['due']
+            todo.category = Category.objects.get(name=request.POST['category'])
+            todo.save()
+            return redirect("/")
+        else:
+            return render(request,'list/home.html',{'categories':categories,'todos':todos,'error':"All fields are necessary!"})
+    else:
+        return render(request,'list/home.html',{'categories':categories,'todos':todos})
+
+
+
+    return render(request,'list/home.html',{'categories':categories,'todos':todos})
